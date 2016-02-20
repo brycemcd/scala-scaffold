659455109001d084e84cc2332c87a8ac208788fa
Bryce McDonnell <bryce@cricketscircle.com>
2016-02-20 13:27:07 -0500

    Add README + Spark
    I use Spark in nearly every scala project I do. It's easy to remove the
    dependency as needed

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,4 +1,11 @@
+package basic
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+
 object Main {
+  //val sc = 
   def main(args: Array[String]) = {
     println("Hello World")
   }
